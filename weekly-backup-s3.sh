a1cde5a2d2f2f1819181e03ac21a787af28bc1d2
capuromeyer <capuromeyer@users.noreply.github.com>
2020-02-07 14:34:52 -0500

    Create weekly-backup-s3.sh

---
+++ weekly-backup-s3.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+# - About: Script that extract MYSQL/MARIADB DB and Back them up locally.
+# - Version: 0.0.1
+# - Date: 05 Feb 2020
+# - Author: Alejandro Capuro
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+echo "///////////////////////////////////////////////////////////////////////////////"
+echo "Daily Backup Script started at | $(date)"
+
+#Load config file
+PATH_TO_PREFLIGHT=$PWD/preflight.sh
+TEMPORAL=$PWD/temporal
+PATH_TO_CONFIG=$PWD/config.sh
+
+source $PATH_TO_PREFLIGHT
+COUNTER_K=0
+
+# For each Databse on the list, mysqldump and archive the file
+echo "====================== Databases ======================"
+for i in ${DBS[@]}
+do
+        echo " ------ Start of $i ------"
+        cd $TEMPORAL
+        NOW=$(date +%Y-%m-%d-%W)
+        DATABASE_FILE="${NOW}_${i}_backup.sql";
+        DATABASE_FILE_ZIP="${DATABASE_FILE}.zip"
+        sudo mysqldump -u $MARIA_USER -p$MARIA_PASSWORD $i > $DATABASE_FILE
+
+        # Compress database file
+        zip $DATABASE_FILE_ZIP $DATABASE_FILE
+        # Cleanup
+        cd $TEMPORAL
+        sudo rm $DATABASE_FILE
+
+        #Move to Backup Directory
+        cd $TEMPORAL
+        sudo mv "$DATABASE_FILE_ZIP" $WEEKLY_BACKUP_DIRECTORY
+        COUNTER_K=$((COUNTER_K+1))
+        echo "-----------------------------------------------"
+done
+echo "======================================================="
+echo "In total $COUNTER_K databases has been backedup"
+#Remove old Backups
+echo "Removing old Files (10 Weeks or Older)"
+cd $WEEKLY_BACKUP_DIRECTORY
+sudo find *.zip -mmin +$((60*24*7*10)) | xargs sudo rm -rfv
+echo "Old files removed"
+echo "Syncronizing Files with S3 Bucket ..."
+s3cmd sync --skip-existing --delete-removed $S3_DIRECTORY $S3_BUCKET
+echo "Script finnished running at | $(date)"
+echo "///////////////////////////////////////////////////////////"
+echo ""
