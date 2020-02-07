ec6461da6844d86c9972ba44fdcc8ca9fd26d2f4
capuromeyer <capuromeyer@users.noreply.github.com>
2020-02-07 14:37:51 -0500

    Create monthly-backup-s3.sh

---
+++ monthly-backup-s3.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+# - About: Script that extract MYSQL/MARIADB DB and Back them up locally.
+# - Version: 0.0.1
+# - Date: 05 Feb 2020
+# - Author: Alejandro Capuro
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+echo "///////////////////////////////////////////////////////////////////////////////"
+echo "Daily Backup Script started at | $(date)"
+
+
+#Load config file
+PATH_TO_PREFLIGHT=$PWD/preflight.sh
+TEMPORAL=$PWD/temporal
+PATH_TO_CONFIG=$PWD/config.sh
+
+source $PATH_TO_PREFLIGHT
+COUNTER_K=0
+
+# For each Databse on the list, mysqldump and archive the file
+echo "====================== Databases ======================"
+for i in ${DBS[@]}
+do
+        echo " ------ Start of $i ------"
+        cd $TEMPORAL
+        mes1=$(date +%m)
+        mes0=$(($mes1 - 1))
+        NOW=$(date +%Y)-"$mes0"
+        DATABASE_FILE="${NOW}_${i}_backup.sql";
+        DATABASE_FILE_ZIP="${DATABASE_FILE}.zip"
+        sudo mysqldump -u 'root' -p'aleluyagod' $i > $DATABASE_FILE
+
+        # Compress database file
+        zip $DATABASE_FILE_ZIP $DATABASE_FILE
+        # Cleanup
+        cd $TEMPORAL
+        sudo rm $DATABASE_FILE
+
+        #Move to Backup Directory
+        cd $TEMPORAL
+        sudo mv "$DATABASE_FILE_ZIP" $MONTHLY_BACKUP_DIRECTORY
+        COUNTER_K=$((COUNTER_K+1))
+        echo "-----------------------------------------------"
+done
+echo "======================================================="
+echo "In total $COUNTER_K databases has been backuped"
+#Remove old Backups
+echo "Removing Files (1.5yrs or older)"
+cd $MONTHLY_BACKUP_DIRECTORY
+sudo find *.zip -mmin +$((60*24*548)) | xargs sudo rm -rfv
+echo "Old files removed"
+echo "Syncronizing Files with S3 Bucket ..."
+s3cmd sync --skip-existing --delete-removed $S3_DIRECTORY $S3_BUCKET
+echo "Script finnished running at | $(date)"
+echo "///////////////////////////////////////////////////////////////////////////////"
+echo ""
