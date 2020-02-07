7c59df8deb09a5fa201eac439f147b3afe05e724
capuromeyer <capuromeyer@users.noreply.github.com>
2020-02-07 14:17:22 -0500

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+# - About: Script for Backuping Database Files
+# - Version: 0.0.1
+# - Date: 5 Feb 2020
+# - Author: Alejandro Capuro
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# List of Databases (MYSQL/MARIADB) to be backuped
+DBS=(
+        "database_01"
+        "database_02"
+        "database_03"
+        "database_04"
+)
+# MariaDB Credentials
+MARIA_USER='root'
+MARIA_PASSWORD='password'
+
+# Local Directory without trailing slash
+LOCAL_BACKUP_DIRECTORY=/Local_Backup
+
+# aws S3 Bucket with trailing slash
+# example s3://your-bucket/
+S3_BUCKET=s3://your-bucket/
+
+# aws S3 Directory inside the Bucket
+# example /Backup_Directory
+S3_DIRECTORY=/S3_Backup_Directory
