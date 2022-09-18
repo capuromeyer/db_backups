c1d68f538a95dc2280ec1db8ba05dff755ad9b19
capuromeyer <capuromeyer@users.noreply.github.com>
2022-09-18 18:15:35 -0500

    Update monthly-backup-s3.sh
    Fix: 09 (september) error,  Bash interpreting digit sequences with leading zeros as octal numbers.

--- monthly-backup-s3.sh
+++ monthly-backup-s3.sh
@@ -27,7 +27,12 @@ do
         cd $TEMPORAL
         mes1=$(date +%m)
         mes0=$(($mes1 - 1))
-        NOW=$(date +%Y)-"$mes0"
+        if [ $mes1 -lt 10 ]
+                then
+                NOW=$(date +%Y)-"0$mes0"
+                else
+                NOW=$(date +%Y)-"$mes0"
+                fi
         DATABASE_FILE="${NOW}_${i}_backup.sql";
         DATABASE_FILE_ZIP="${DATABASE_FILE}.zip"
         sudo mysqldump -u 'root' -p'aleluyagod' $i > $DATABASE_FILE
